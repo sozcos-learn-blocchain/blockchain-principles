ee54031cc8d677de596a2008849beab2623aa597
Elsen <poozs@qq.com>
2023-10-11 17:30:34 +0800

    添加区块链算法模块

---
+++ block-chain/block-chain-algorithm/src/main/java/com/sozcos/App.java
@@ -0,0 +1,13 @@
+package com.sozcos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ block-chain/block-chain-algorithm/src/main/java/com/sozcos/merkel/MerkelTree.java
@@ -0,0 +1,17 @@
+package com.sozcos.merkel;
+
+/**
+ * 默克尔数
+ *
+ * @author Elsen 2023/10/11
+ */
+public class MerkelTree {
+        /*
+        相关文章:
+            https://zhuanlan.zhihu.com/p/39271872
+            https://blog.csdn.net/qq_34902437/article/details/120911504
+
+            java 实现 默克尔树:
+            https://zhuanlan.zhihu.com/p/273457592
+     */
+}
