898b2fbdaf858b30e4de56e36719f8e9faa94ca4
Elsen <poozs@qq.com>
2023-10-11 17:00:14 +0800

    添加区块链原理模块

---
+++ block-chain/P1-blockchain-principles/README.md
@@ -0,0 +1,3 @@
+# 区块链技术原理
+
+[博客园——区块链技术原理](https://www.cnblogs.com/davidzhu/p/16396812.html)
---
+++ block-chain/P1-blockchain-principles/src/main/java/com/sozcos/App.java
@@ -0,0 +1,10 @@
+package com.sozcos;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
